51cb730089f65ef64d29cb1a7708af7d66fcea23
Mike Sweet <sweetmichael_j@yahoo.com>
2018-03-04 14:06:32 -0500

    built searchBar.js initial build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div class="App">
       <!-- Add a SearchBar component -->
              <div class="App-playlist">
---
+++ src/components/searchBar/searchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './searchBar.css';
+
+export class SearchBar extends React.Component {
+  constructor (props) {
+    super()
+  }
+}
+
+render () {
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <a>SEARCH</a>
+    </div>
+  )
+};
+
+export default SearchBar;
