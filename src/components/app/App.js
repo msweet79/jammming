35b91b45628ddc8116a843e4df2e1676e694554a
Mike Sweet <sweetmichael_j@yahoo.com>
2018-03-04 11:47:04 -0500

    added imports to the app.js file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
+
+import SearchBar from './components/SearchBar/SearchBar';
+import SearchResults from './components/SearchResults/SearchResults';
+import Playlist from './components/playlist/playlist';
 
 class App extends Component {
   render() {
     return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+          <div class="App">
+      <!-- Add a SearchBar component -->
+             <div class="App-playlist">
+      <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component -->
+            </div>
+          </div>
+      </div>
+
+/*original code block
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +30,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+*/
     );
   }
 }
