7a24d5f86681da7432953770e840d6095597a18d
Mike Sweet <sweetmichael_j@yahoo.com>
2018-03-04 18:45:21 -0500

    updated the add and remove song features

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './playlist.css';
+import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
   constructor (props) {
-    super()
-  }
-}
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  };
+
 
 render () {
   return (
-    <div class="Playlist">
-      <input value="New Playlist"/>
-      <!-- Add a TrackList component -->
-      <a class="Playlist-save">SAVE TO SPOTIFY</a>
-    </div>
-  );
+      <div className="Playlist">
+        <input defaultValue={this.props.name} onChange={this.handleNameChange}/>
+        <TrackList tracks={this.props.tracks} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
+        <a className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</a>
+      </div>
+    );
+  };
 }
 
 export default Playlist;
--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './track.css'
 
-class Track extends Component {
+class Track extends React.Component {
   constructor (props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
 
+  addTrack () {
+    this.props.onAdd(this.props.track);
+  };
+
+  removeTrack() {
+    this.props.onRemove(this.props.track);
   }
 
 renderAction () {
   if (this.props.isRemoved) {
     return <a className='Track-action' onClick={this.removeTrack}>-</a>
   };
+};
 
-  //end of removal - feature
-}
 render () {
   return (
-    <div className="Track">
-      <div className="Track-information">
-        <h3><!-- track name will go here --></h3>
-        <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
+        </div>
+        <a className="Track-action">{!this.props.isRemoval ? <div onClick={this.addTrack}>+</div> : <div onClick={this.removeTrack}>-</div>}</a>
       </div>
-      <a className="Track-action"><!-- + or - will go here --></a>
-    </div>
-  );
-  //end of render method
-};
-//end of class Track component
+    );
+  };
 }
 
 export default Track;
