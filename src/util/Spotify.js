aaf7edd0910759f2d71fbd4a8ea10925ef43dc09
Michael Sweet <sweetmichael_j@yahoo.com>
2018-03-05 10:26:06 -0500

    Added Spotify search fetch()

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -23,7 +23,34 @@ const Spotify = {
           const requestingUrl = "https://accounts.spotify.com/authorize/?client_id=" + cID + "&response_type=token&redirect_uri=" + redirectUri;
           window.location = requestingUrl;
         };
+      },
+  
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
       }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.tracks) {
+        return jsonResponse.tracks.items.map(track => {
+          return ({
+            id: track.id,
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri
+          });
+        })
+      } else {
+        return [];
+      }
+    })
+  },
+  
+  
 
     }
 
