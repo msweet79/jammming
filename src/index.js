101498508f967326364c1ff4b6d54fbadab5b797
Mike Sweet <sweetmichael_j@yahoo.com>
2018-03-05 21:51:33 -0500

    changed import React in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/app';
+import App from './components/app/app.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
