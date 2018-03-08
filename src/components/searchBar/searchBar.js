7f77e66bca9af506d38cc3218c2c6f780be05f5c
Michael Sweet <sweetmichael_j@yahoo.com>
2018-03-08 09:41:22 -0500

    Added comments and additional search term code block

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -4,19 +4,28 @@ import './searchBar.css';
 export class SearchBar extends React.Component {
   constructor (props) {
     super(props);
+    //Set default state
     this.state = {searchTerm: ""};
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   };
 
+  //Bind methods to SearchBar
   search() {
     this.props.onSearch(this.state.searchTerm);
   };
 
+//Change search terms when form is changes
 //e = event
 handleTermChange(e) {
   this.setState({searchTerm: e.target.value});
 }
+  
+  //**  May need to remove this!!!
+  // Pass the search term to search function
+  search () {
+    this.props.onSearch(this.state.term)
+  }
 
 render () {
   return (
