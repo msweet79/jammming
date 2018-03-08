443f58a021500f947502c50db4866fec6e58dee1
Michael Sweet <sweetmichael_j@yahoo.com>
2018-03-08 09:49:20 -0500

    Comment added.  Re-org to make easier to read

--- src/components/trackList/trackList.js
+++ src/components/trackList/trackList.js
@@ -4,16 +4,21 @@ import Track from '../track/track';
 
 class TrackList extends React.Component {
 
+  //Render tracks from the track aray
   render() {
     return (
       <div className="TrackList">
         { this.props.tracks.map(track => {
-            return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
+            return <Track track={track} 
+            key={track.id} 
+            onAdd={this.props.onAdd} 
+            onRemove={this.props.onRemove} 
+            isRemoval={this.props.isRemoval} />
           })
         }
       </div>
-    );
-  };
+    )
+  }
 }
 
 export default TrackList;
